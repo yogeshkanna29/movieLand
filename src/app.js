15ce840f9426838309f55e7fccc2634a66ec0e08
Yogesh <k.yogesh@machinemaze.com>
2024-10-13 19:08:43 +0530

    commits

--- src/app.js
+++ src/app.js
@@ -3,27 +3,24 @@ import "./app.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=3f611ae2";
-
-// const movie1 = {
-//   Title: "Italian Spiderman",
-//   Year: "2007",
-//   imdbID: "tt2705436",
-//   Type: "movie",
-//   Poster:
-//     "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
-// };
+const API_URL = "https://www.omdbapi.com/?apikey=3f611ae2";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
+    try {
+      const response = await fetch(`${API_URL}&s=${encodeURIComponent(title)}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setMovies(data.Search || []);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setMovies([]);
+    }
   };
 
   useEffect(() => {
@@ -43,10 +40,10 @@ const App = () => {
         <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
 
-      {movies?.length > 0 ? (
+      {movies.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
